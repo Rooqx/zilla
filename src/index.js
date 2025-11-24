89e8919606a91769b8429a70a827d7c7a982667c
Rooqx <ashfaga2005f@gmail.com>
2025-11-24 22:23:29 +0100

    tailwind now works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.jsx";
 import "./global.css";
+import App from "./app.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
